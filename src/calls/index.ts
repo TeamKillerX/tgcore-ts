cd5b4b3cc7b955f9ba5bb6933e589baf0518105c
Randy SL <killerx@randydev.my.id>
2026-02-25 02:19:23 +0700

    Create index.ts

---
+++ src/calls/index.ts
@@ -0,0 +1,26 @@
+/*
+# Copyright 2026 Randy W
+# Licensed under the Apache License, Version 2.0
+
+# Github Author: https://github.com/TeamKillerX/
+# Code: @zxyeor
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+import { SendMessageBuilder } from "./sendMessage"
+
+export class CallMethods {
+  constructor(private http: any) {}
+
+  sendMessage() {
+    return new SendMessageBuilder(
+      this.http,
+      "/api/v2/sendMessage"
+    )
+  }
+}
