3d9fba6e1c594d03ad4e00670f0ff6690833432b
Randy SL <killerx@randydev.my.id>
2026-02-25 05:39:38 +0700

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
-import { Http } from "./http"
-import { RawMethods } from "./raw/methods"
-import { CallMethods } from "./calls"
+import { Http } from "./http.js"
+import { RawMethods } from "./raw/methods.js"
+import { CallMethods } from "./calls.js"
 
 export type ClientOptions = {
   api_key: string
