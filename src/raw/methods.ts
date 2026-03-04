b3a5dccd3653d906b733a003fe7f8ffd86d5ec61
Randy SL <killerx@randydev.my.id>
2026-03-04 17:09:35 +0700

    _

--- src/raw/methods.ts
+++ src/raw/methods.ts
@@ -9,10 +9,16 @@ export class RawMethods {
     parse_mode?: string
     reply_markup?: any
   }) {
-    return this.http.post("/api/v2/sendMessage", params)
+    return this.http.post({
+      path: "/api/v2/sendMessage",
+      body: params
+    })
   }
 
   getMe() {
-    return this.http.post("/api/v2/getMe", {})
+    return this.http.post({
+      path: "/api/v2/getMe",
+      body: {}
+    })
   }
 }
