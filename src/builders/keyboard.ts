b542cecf49a1a86b2e8936c2f015603521169bf5
Randy SL <killerx@randydev.my.id>
2026-03-04 16:52:22 +0700

    _

--- src/builders/keyboard.ts
+++ src/builders/keyboard.ts
@@ -12,33 +12,8 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 */
 
-export type InlineKeyboardButton = {
-  text: string
-  url?: string
-  style?: string
-  callback_data?: string
-  web_app?: { url: string }
-  login_url?: {
-    url: string
-    forward_text?: string
-    bot_username?: string
-    request_write_access?: boolean
-  }
-  switch_inline_query?: string
-  switch_inline_query_current_chat?: string
-  switch_inline_query_chosen_chat?: {
-    query?: string
-    allow_user_chats?: boolean
-    allow_bot_chats?: boolean
-    allow_group_chats?: boolean
-    allow_channel_chats?: boolean
-  }
-  copy_text?: { text: string }
-}
-
-export type InlineKeyboardMarkup = {
-  inline_keyboard: InlineKeyboardButton[][]
-}
+import { InlineKeyboardButton } from "../types/InlineKeyboardButton"
+import { InlineKeyboardMarkup } from "../types/InlineKeyboardMarkup"
 
 export class KeyboardBuilder {
   private keyboard: InlineKeyboardButton[][] = [[]]
